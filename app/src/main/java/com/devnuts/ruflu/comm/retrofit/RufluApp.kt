8a2d3682be7855bf59cfde35320ccdaf2df85e5b
dustjq1004 <73459956+dustjq1004@users.noreply.github.com>
2022-07-08 14:50:39 +0900

    initial PULL REQUEST TEMPLATE

--- app/src/main/java/com/devnuts/ruflu/comm/retrofit/RufluApp.kt
+++ app/src/main/java/com/devnuts/ruflu/comm/retrofit/RufluApp.kt
@@ -26,7 +26,7 @@ class RufluApp : Application() {
         lateinit var retrofit: Retrofit
         lateinit var appNotification: AppNotification
         lateinit var sharedPreference: CustomSharedPreference
-        val url = "http://172.30.1.57"
+        val url = "http://192.168.0.15"
         val port = 8005
     }
 
