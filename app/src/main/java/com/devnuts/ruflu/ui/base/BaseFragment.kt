c571b81159418accdc0b2bc411c1cfe10a119cf9
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-12-20 21:45:00 +0900

    refactor/#6 [feat] : 생명주기 로그를 찍기 위한 BaseFragment 구현

---
+++ app/src/main/java/com/devnuts/ruflu/ui/base/BaseFragment.kt
@@ -0,0 +1,65 @@
+package com.devnuts.ruflu.ui.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import timber.log.Timber
+
+class BaseFragment: Fragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i("onAttach call")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Timber.i("onCreateView call")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.i("onViewCreated call")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart call")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume call")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause call")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop call")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Timber.i("onDestroyView call")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy call")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i("onDetach call")
+    }
+}
