5d3b0ce2f64b47a5101fef82f45ec7509174b2c5
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-16 10:18:17 +0900

    refactor/#6 [feat] : 온보딩 gender 구현

---
+++ app/src/main/java/com/devnuts/ruflu/ui/model/onboarding/GenderUIModel.kt
@@ -0,0 +1,10 @@
+package com.devnuts.ruflu.ui.model.onboarding
+
+import com.devnuts.ruflu.ui.model.CellType
+import com.devnuts.ruflu.ui.model.Model
+
+data class GenderUIModel(
+    override val type: CellType = CellType.GENDER_CEL,
+    val resourceId: Int,
+    val name: String,
+): Model(type)
--- app/src/main/java/com/devnuts/ruflu/ui/onboarding/fragment/one/OnboardingGenderFragment.kt
+++ app/src/main/java/com/devnuts/ruflu/ui/onboarding/fragment/one/OnboardingGenderFragment.kt
@@ -2,19 +2,47 @@ package com.devnuts.ruflu.ui.onboarding.fragment.one
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.devnuts.ruflu.R
 import com.devnuts.ruflu.databinding.FragmentOnboardingGenderBinding
+import com.devnuts.ruflu.ui.adapter.ModelRecyclerViewAdapter
+import com.devnuts.ruflu.ui.model.CellType
+import com.devnuts.ruflu.ui.model.Model
+import com.devnuts.ruflu.ui.model.onboarding.GenderUIModel
+import com.devnuts.ruflu.util.listener.ModelAdapterListener
 
 /* 온보딩 - 1 : 성별  */
 class OnboardingGenderFragment : Fragment() {
     private var _binding: FragmentOnboardingGenderBinding? = null
     private val binding get() = _binding!!
     private val viewModel: OnboardingGenderViewModel by viewModels()
+
+    private lateinit var genderLayoutManager: RecyclerView.LayoutManager
+
+    private val genderAdapter: ModelRecyclerViewAdapter<Model> by lazy {
+        ModelRecyclerViewAdapter(object : ModelAdapterListener {
+            override fun onClick(view: View, model: Model, position: Int) {
+                when(model.type) {
+                    CellType.GENDER_CEL -> {
+                        val rv = binding.rvGender
+                        viewModel.genderSingleChoice(rv, position)
+                    }
+                    else -> {}
+                }
+            }
+
+            override fun onTouch(view: View, model: Model, event: MotionEvent) {
+            }
+            override fun onSwipe(position: Int, direction: Int) {}
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,16 +57,46 @@ class OnboardingGenderFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         progressBar()
-        initializeView()
+        setupNavigation()
+
+        initializeObserve()
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        genderAdapter.submitList(
+            listOf(
+                GenderUIModel(resourceId = R.drawable.onboarding_female_selector, name = "여성"),
+                GenderUIModel(resourceId = R.drawable.onboarding_male_selector, name = "남성")
+            )
+        )
+
+        binding.rvGender.adapter = genderAdapter
+        genderLayoutManager = GridLayoutManager(requireContext(), 2)
+        genderLayoutManager = object  : GridLayoutManager(requireContext(), 2) {
+            override fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
+                lp.width = ((width - 50) / spanCount)
+                return true
+            }
+        }
+
+        binding.rvGender.setHasFixedSize(true)
+        binding.rvGender.layoutManager = genderLayoutManager
+    }
+
+    private fun initializeObserve() {
+        viewModel.gender.observe(viewLifecycleOwner) {
+            binding.btnGender.isSelected = true
+        }
     }
 
     private fun progressBar() {
-        val curProgressBar = binding.pbLoading
-        curProgressBar.progress = 16
+        binding.pbLoading.progress = 0
     }
 
-    private fun initializeView() {
-        binding.genderNextBnt.setOnClickListener {
+
+    private fun setupNavigation() {
+        binding.btnGender.setOnClickListener {
             findNavController().navigate(R.id.action_genderFragment_to_birthFragment)
         }
     }
