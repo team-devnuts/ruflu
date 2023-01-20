d163b54e6f360a924b87ce57ab6c11b3754bb0db
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-20 17:59:48 +0900

    refactor/#6 [feat] : chat message model 생성

--- app/src/main/java/com/devnuts/ruflu/ui/model/chat/ChatMessage.kt
+++ app/src/main/java/com/devnuts/ruflu/ui/model/chat/ChatMessage.kt
@@ -1,3 +1,7 @@
 package com.devnuts.ruflu.ui.model.chat
 
-data class ChatMessage(val userName: String, val msg: String, val imgUrl: String)
+data class ChatMessage(
+    val userName: String,
+    val msg: String,
+    val imgUrl: String,
+)
