5af69800ef87c96febd3cbf042e9583726aec086
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-01-24 15:10:51 +0900

    refactor/#6 [rename] : remote api 메서드명 변경

--- app/src/main/java/com/devnuts/ruflu/util/ServerAPI.kt
+++ app/src/main/java/com/devnuts/ruflu/util/ServerAPI.kt
@@ -2,30 +2,28 @@ package com.devnuts.ruflu.util
 
 import com.devnuts.ruflu.data.api.response.card.UserModel
 import com.devnuts.ruflu.ui.model.chat.ChatRoom
-import com.devnuts.ruflu.ui.model.home.UserUIModel
-import com.devnuts.ruflu.ui.model.like.SomeUser
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ServerAPI {
 
     @GET("home/userCardList")
-    fun getUserCardList(): Call<List<UserModel>>
+    fun getUserList(): Call<List<UserModel>>
 
     @FormUrlEncoded
     @POST("home/ins/hate")
-    fun insertHateUserCard(@FieldMap param: HashMap<String, String>): Call<String>
+    fun addUserInMyHateList(@FieldMap param: HashMap<String, String>): Call<String>
 
     @FormUrlEncoded
     @POST("home/ins/like")
-    fun insertLikeUserCard(@FieldMap param: HashMap<String, String>): Call<String>
+    fun addUserInMyLikeList(@FieldMap param: HashMap<String, String>): Call<String>
+
+    @GET("home/seLv1List")
+    fun getLikeMeList(): Call<ArrayList<UserModel>>
 
     @FormUrlEncoded
-    @POST("home/chg/star")
-    fun changeRatingToUser(
-        @Field("star") rating: Float,
-        @Field("userId") userId: String
-    ): Call<String>
+    @POST("home/seLv1/like")
+    fun addUserInMyMatchList(@Field("userId") userId: String): Call<String>
 
     @FormUrlEncoded
     @POST("main/loca/udt")
@@ -38,24 +36,20 @@ interface ServerAPI {
     @POST("main/token/udt")
     fun executeFcmServiceToken(@Field("token") token: String): Call<String>
 
-    @GET("home/seLv1List")
-    fun getSeLv1User(): Call<ArrayList<UserModel>>
-
-    @GET("home/seLv1/userDtl/{userId}")
-    fun getSeLv1UserDtl(@Path("userId") userId: String): Call<SomeUser>
-
-    @FormUrlEncoded
-    @POST("home/seLv1/like")
-    fun insertSeLikeLv2(@Field("userId") userId: String): Call<String>
 
     @GET("home/seLv2List")
-    fun getSeLv2User(): Call<ArrayList<UserModel>>
-
-    @FormUrlEncoded
-    @POST("home/remove/like")
-    fun deleteLikeUser(@Field("toUser") userId: String?): Call<String>
+    fun getUserMatchedWithMeList(): Call<ArrayList<UserModel>>
 
     // 채팅 api
     @GET("chat/list")
     fun getMyChattingRoomList(): Call<List<ChatRoom>>
 }
+
+
+
+//    @GET("home/seLv1/userDtl/{userId}")
+//    fun getUserDetail(@Path("userId") userId: String): Call<SomeUser>
+
+//    @FormUrlEncoded
+//    @POST("home/remove/like")
+//    fun deleteSomeUser(@Field("toUser") userId: String?): Call<String>
