b24fe3513b24982b22dd329b7fbfdf1a8993f08f
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-12 15:47:26 +0900

    refactor/#6 [rename] : Some과 Like 혼동되는 이름을 다시 정리
    변경 전 -> Like > Some, Match
    변경 후 -> Some > Like, Match

--- app/src/main/java/com/devnuts/ruflu/ui/some/fragment/LikeFragment.kt
+++ app/src/main/java/com/devnuts/ruflu/ui/some/fragment/LikeFragment.kt
@@ -1,8 +1,7 @@
-package com.devnuts.ruflu.ui.like.fragment
+package com.devnuts.ruflu.ui.some.fragment
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
@@ -12,11 +11,11 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devnuts.ruflu.R
-import com.devnuts.ruflu.databinding.FragmentSomeBinding
+import com.devnuts.ruflu.databinding.FragmentLikeBinding
 import com.devnuts.ruflu.ui.adapter.SwipeAdapter
 import com.devnuts.ruflu.ui.common.UserDetailFragment
-import com.devnuts.ruflu.ui.like.listener.SomeTouchHelperCallback
-import com.devnuts.ruflu.ui.like.viewmodel.SomeViewModel
+import com.devnuts.ruflu.ui.some.SomeTouchHelperCallback
+import com.devnuts.ruflu.ui.some.viewmodel.LikeViewModel
 import com.devnuts.ruflu.ui.model.Model
 import com.devnuts.ruflu.ui.model.home.UserUIModel
 import com.devnuts.ruflu.util.listener.ModelAdapterListener
@@ -25,17 +24,17 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @AndroidEntryPoint
-class SomeFragment : Fragment() {
+class LikeFragment : Fragment() {
     /* 정리 필요 */
     private lateinit var userDetailFragment: UserDetailFragment
     private lateinit var childFragmentTransaction: FragmentTransaction
     private lateinit var callback: OnBackPressedCallback
 
-    private var _binding: FragmentSomeBinding? = null
+    private var _binding: FragmentLikeBinding? = null
     val binding get() = _binding!!
-    private val someViewModel: SomeViewModel by viewModels()
+    private val likeViewModel: LikeViewModel by viewModels()
 
-    private val someAdapter: SwipeAdapter<Model> by lazy {
+    private val likeAdapter: SwipeAdapter<Model> by lazy {
         SwipeAdapter(object : ModelAdapterListener {
             override fun onClick(view: View, model: Model, position: Int) {
                 userDetailFragment = UserDetailFragment(model as UserUIModel)
@@ -54,7 +53,7 @@ class SomeFragment : Fragment() {
             override fun onSwipe(position: Int, direction: Int) {
                 /** 32 right 좋아요, 16 left  싫어요 **/
                 if (direction == 32) {
-                    someViewModel.addUserInMyMatchList(someViewModel.getSomeUser(position)!!.userId)
+                    likeViewModel.addUserInMyMatchList(likeViewModel.getSomeUser(position)!!.userId)
                 }
                 // 현재 임시
                 //someViewModel.userInfo.value?.toMutableList()?.remove(someViewModel.getSomeUser(position))
@@ -68,7 +67,7 @@ class SomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLikeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -80,24 +79,22 @@ class SomeFragment : Fragment() {
     }
 
     private fun setupAdapter() {
-        binding.rvSome.layoutManager =
+        binding.rvLike.layoutManager =
             LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
-        binding.rvSome.adapter = someAdapter
-        val helper = ItemTouchHelper(SomeTouchHelperCallback(someAdapter))
-        helper.attachToRecyclerView(binding.rvSome)
+        binding.rvLike.adapter = likeAdapter
+        val helper = ItemTouchHelper(SomeTouchHelperCallback(likeAdapter))
+        helper.attachToRecyclerView(binding.rvLike)
     }
 
     private fun initObserve() {
-        Log.d("flow", "SomeFragment IN!!!!!!")
         this.lifecycleScope.launch {
-            someViewModel.getLikeMeUserList()
+            likeViewModel.getLikeMeUserList()
         }
 
         this.lifecycleScope.launch {
-            someViewModel.userInfo.collect {
-                //Log.d("flow", "${it.get(0).type}")
+            likeViewModel.userInfo.collect {
                 val model = it as List<Model>
-                someAdapter.submitList(model)
+                likeAdapter.submitList(model)
             }
         }
     }
